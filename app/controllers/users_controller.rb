26d5811333ff6fee16f3e011ffc55b645fa6b7a2
eteRnaL198 <ikuya313198@gmail.com>
2021-08-26 09:04:53 +0900

    add users edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,18 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def exit
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :profile, :profile_image) # データをコントローラ内でのみ取り出せるようにする
   end
 end
